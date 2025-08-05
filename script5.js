d410052c7b60964337e9b3b8b8574bf4e0d365f0
ankitha0613 <kengurinirmala@gmail.com>
2025-08-05 19:24:00 +0530

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,37 @@
+//Separate Phone Numbers
+const phoneNumbers = [
+    "+919876543210",
+    "+14085551234",
+    "+918888777666",
+    "+12345678901",
+    "+917654321098",
+];
+
+let indianNumbers = phoneNumbers.filter((number) =>
+    number.startsWith("+91")
+);
+let usNumbers = phoneNumbers.filter((number) =>
+    number.startsWith("+1")
+);
+
+let result = {
+    indianNumbers,
+    usNumbers,
+};
+
+console.log(result);
+//Insert Product in Cart
+let cart = [101, 102, 104];
+let newProduct = 103;
+
+cart.splice(1, 0, newProduct);
+
+console.log(cart);
+
+//Insert Student in Wating List
+let waitingList = ["Ali", "John", "Sara"];
+let newStudent = "Aarav";
+
+waitingList.push(newStudent);
+
+console.log(waitingList);
