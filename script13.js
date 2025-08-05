ec30ec652de30b9ee4d8deda8dff8027d0fed6f8
ankitha0613 <kengurinirmala@gmail.com>
2025-08-05 19:27:49 +0530

    Add files via upload

---
+++ script13.js
@@ -0,0 +1,18 @@
+function longestProductiveStreak(days, k) {
+    let maxStreak = 0;
+
+    for (let i = 0; i < days.length; i++) {
+        let zeros = 0;
+        for (let j = i; j < days.length; j++) {
+            if (days[j] === 0) zeros++;
+            if (zeros > k) break;
+            maxStreak = Math.max(maxStreak, j - i + 1);
+        }
+    }
+
+    return maxStreak;
+}
+
+let days = [1, 0, 1, 1, 0, 0, 1, 1, 1, 0];
+let k = 2;
+console.log(longestProductiveStreak(days, k));
